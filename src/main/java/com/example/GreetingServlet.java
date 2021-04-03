172c39f97fc68f4445de359dbffac2728656602e
Hantsy Bai <hantsy@gmail.com>
2021-04-03 16:05:54 +0800

    chore: clean codes

--- src/main/java/com/example/GreetingServlet.java
+++ src/main/java/com/example/GreetingServlet.java
@@ -5,32 +5,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = "/GreetingServlet")
 public class GreetingServlet extends HttpServlet {
-
+    private static final Logger LOGGER = Logger.getLogger(GreetingServlet.class.getName());
     private final static String PAGE_HEADER = "<html><head><title>Greeting Servlet</title></head><body>";
     private final static String PAGE_FOOTER = "</body></html>";
-
+    
     @Inject
     GreetingService greetingService;
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         var name = req.getParameter("name");
         resp.setContentType("text/html");
         var writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-
-        try {
-            writer.println("<p>" + greetingService.buildGreetingMessage(name) + "</p>");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        
+        // write message to http response
+        writer.println("<p>" + greetingService.buildGreetingMessage(name) + "</p>");
+        
         writer.println(PAGE_FOOTER);
         writer.close();
     }
-
+    
 }
