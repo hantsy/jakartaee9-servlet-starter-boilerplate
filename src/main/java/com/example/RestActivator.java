7e1b61efe12a28861f9683fe3dc028181cb4b65d
Hantsy Bai <hantsy@gmail.com>
2021-04-03 12:53:34 +0800

    chore: clean codes

---
+++ src/main/java/com/example/RestActivator.java
@@ -0,0 +1,13 @@
+package com.example;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class RestActivator extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(GreetingService.class);
+    }
+}
