048dd3ab234b428b36cc150809157e2cba85c960
hantsy <hantsy@gmail.com>
2022-06-05 12:13:27 +0800

    chore: add embed maven jetty plugin profile.

--- src/main/java/com/example/RestActivator.java
+++ src/main/java/com/example/RestActivator.java
@@ -1,13 +1,14 @@
 package com.example;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
 
 import java.util.Set;
 
 @ApplicationPath("/api")
-public class RestActivator extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        return Set.of(GreetingResource.class);
+public class RestActivator extends ResourceConfig {
+    public RestActivator() {
+        packages("com.example");
     }
+
 }
